8e51d427cae1b84f50c1b0ca5ae45602c65042b1
nkjsy <nkjsy@live.cn>
2020-02-22 21:53:21 +0800

    define the database and functions for connection and closing

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from . import db
 
 
 def create_app(test_config=None):
@@ -7,7 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
     )
 
     if test_config is None:
@@ -27,5 +28,8 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    # register the database functions
+    db.init_app(app)
 
     return app
